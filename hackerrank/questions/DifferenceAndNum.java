5c76582828b0e4aab44dc737864bd695b1b1cbb8
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-03-30 21:44:10 +0530

    Added Problem Description
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- hackerrank/questions/DifferenceAndNum.java
+++ hackerrank/questions/DifferenceAndNum.java
@@ -1,5 +1,23 @@
 package questions;
 
+/*
+Create the function that accepts an integer array ‘arr’, its length and two integer variables ‘num’ and ‘diff’. Implement this function to find and return the number of elements of ‘arr’ having an absolute difference of less than or equal to ‘diff’ with ‘num’. Note: In case there is no element in ‘arr’ whose absolute difference with ‘num’ is less than or equal to ‘diff’, return -1.
+
+Input Format
+size of array input array two integers num and diff
+
+Output Format
+print count of elements of ‘arr’ having absolute difference of less than or equal to ‘diff’
+
+Sample Input 0
+6
+12 3 14 56 77 13
+13
+2
+
+Sample Output 0
+3*/
+
 import java.util.Scanner;
 
 public class DifferenceAndNum {
